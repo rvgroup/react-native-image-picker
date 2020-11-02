836e989a9a43be59eb2309ce60a7dcf681039880
rudenkovitalij <rudenkovitalij@gmail.com>
2020-11-02 15:43:31 +0300

    * fix

--- android/src/main/java/com/imagepicker/ImagePickerModule.java
+++ android/src/main/java/com/imagepicker/ImagePickerModule.java
@@ -133,7 +133,7 @@ public class ImagePickerModule extends ReactContextBaseJavaModule implements Act
         callback.invoke(getVideoResponseMap(uri, options, reactContext));
     }
 
-    boolean _isMyRequest(requestCode) {
+    boolean _isMyRequest(String requestCode) {
         return requestCode == REQUEST_LAUNCH_IMAGE_CAPTURE
             || requestCode == REQUEST_LAUNCH_IMAGE_LIBRARY
             || requestCode == REQUEST_LAUNCH_VIDEO_LIBRARY
